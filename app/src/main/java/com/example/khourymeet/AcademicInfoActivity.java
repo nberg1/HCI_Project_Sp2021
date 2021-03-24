be49201001177576e8c82024047007437293ef17
Gerard Otalora <gerard.otca@gmail.com>
2021-03-23 21:21:30 -0400

    Initial commit

---
+++ app/src/main/java/com/example/khourymeet/AcademicInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.khourymeet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AcademicInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_academic_info);
+    }
+}
---
+++ app/src/main/java/com/example/khourymeet/PersonalInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.khourymeet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_info);
+    }
+}
