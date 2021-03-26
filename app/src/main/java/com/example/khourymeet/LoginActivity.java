a606674f638f4307c3d062ab4bb905fe699a8cce
Gerard Otalora <gerard.otca@gmail.com>
2021-03-25 21:33:44 -0400

    Login done

--- app/src/main/java/com/example/khourymeet/LoginActivity.java
+++ app/src/main/java/com/example/khourymeet/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.khourymeet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    // When selecting the login option user is redirected to LoginActivity
+    public void onClickNext(View view) {
+        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
