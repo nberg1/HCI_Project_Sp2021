9e8051c0ab518341795d8fa3a3ce56f070dfb8ac
nberg002 <berg.n@northeastern.edu>
2021-04-06 14:20:52 -0400

    adding bottom navigation bar

--- app/src/main/java/com/example/khourymeet/fragments/HomeFragment.java
+++ app/src/main/java/com/example/khourymeet/fragments/HomeFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -63,4 +65,6 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+
 }
