9ff08a7cacf3378ba8d08ba5ab5cf579c92a7632
Gerard Otalora <gerard.otca@gmail.com>
2021-04-06 21:49:02 -0400

    Clean imports

--- app/src/main/java/com/example/khourymeet/PersonalInfoActivity.java
+++ app/src/main/java/com/example/khourymeet/PersonalInfoActivity.java
@@ -1,29 +1,16 @@
 package com.example.khourymeet;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.InstanceIdResult;
 
 public class PersonalInfoActivity extends AppCompatActivity {
 
