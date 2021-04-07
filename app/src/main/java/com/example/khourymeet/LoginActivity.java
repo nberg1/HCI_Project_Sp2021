dfc6aab7382a6524cea986b9c93f277342fee72f
Gerard Otalora <gerard.otca@gmail.com>
2021-04-06 21:48:04 -0400

    Login works

--- app/src/main/java/com/example/khourymeet/LoginActivity.java
+++ app/src/main/java/com/example/khourymeet/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.khourymeet;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -15,6 +16,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,7 +29,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private DatabaseReference databaseReference;
-    private EditText username;
+    private EditText userName;
     private EditText password;
 
     @Override
@@ -37,116 +39,37 @@ public class LoginActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
         databaseReference = FirebaseDatabase.getInstance().getReference();
-        username = findViewById(R.id.username_input);
+        userName = findViewById(R.id.username_input);
         password = findViewById(R.id.password_input);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (currentUser != null) {
-            Toast.makeText(getApplicationContext(), "User logged in", Toast.LENGTH_LONG).show();
-            String name = currentUser.getDisplayName();
-            GetUser getUser = new GetUser();
-            getUser.getUser(name);
-        }
-    }
+    public void onClick(View view) {
 
-    public void onClickLogin(View view) {
+        final String userNameString = userName.getText().toString();
+        final String passwordString = password.getText().toString();
 
-        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (currentUser != null) {
-            Toast.makeText(getApplicationContext(), "User logged in", Toast.LENGTH_LONG).show();
-            String name = currentUser.getDisplayName();
-            GetUser getUser = new GetUser();
-            getUser.getUser(name);
-
+        if (userNameString.equals("")) {
+            Toast.makeText(getApplicationContext(), "Please enter a username", Toast.LENGTH_LONG).show();
+        } else if (passwordString.equals("")) {
+            Toast.makeText(getApplicationContext(), "Please enter a password", Toast.LENGTH_LONG).show();
         } else {
-            final String userNameString = username.getText().toString();
-            final String passwordString = password.getText().toString();
-
-            if (userNameString.equals("")) {
-                Toast.makeText(getApplicationContext(), "Please enter a username", Toast.LENGTH_LONG).show();
-            } else if (passwordString.equals("")) {
-                Toast.makeText(getApplicationContext(), "Please enter a password", Toast.LENGTH_LONG).show();
-            } else {
-                firebaseAuth.signInWithEmailAndPassword(userNameString, passwordString)
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    FirebaseUser user = firebaseAuth.getCurrentUser();
-                                    String name = user.getDisplayName();
-                                    GetUser getUser = new GetUser();
-                                    getUser.getUser(name);
-                                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                                    startActivity(intent);
-                                } else {
-                                    Toast.makeText(getApplicationContext(), "User does not exist. Please register a new user.", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-            }
-        }
-    }
-
-    private class GetUser implements Runnable {
-
-        private String username;
-        private User user;
-
-        public GetUser() {
-        }
-
-        public void getUser(String username) {
-            this.username = username;
-            this.runInNewThread();
-        }
-
-        @Override
-        public void run() {
-            FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(LoginActivity.this, new OnSuccessListener<InstanceIdResult>() {
+            databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
-                public void onSuccess(final InstanceIdResult instanceIdResult) {
-                    final String token = instanceIdResult.getToken();
-                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot snapshot) {
-                            if (snapshot.hasChild(username)) {
-                                user = snapshot.child(username).getValue(User.class);
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.child(userNameString).exists() && snapshot.child(userNameString).child("password").getValue().equals(passwordString)) {
+                        Toast.makeText(getApplicationContext(), "Successful Login!", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Account does not exist. Please register a new user.", Toast.LENGTH_SHORT).show();
+                    }
+                }
 
-                                if (user.getToken() != null && !user.getToken().equals(token)) {
-                                    user.setToken(token);
-                                    databaseReference.child("users").child(user.getUsername()).setValue(user);
-                                }
-                                startIntent(user);
-                            } else {
-                                Toast.makeText(getApplicationContext(), "User exists in Auth but not in the Database", Toast.LENGTH_SHORT).show();
-                            }
-                            startIntent(user);
-                        }
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-                            Toast.makeText(getApplicationContext(), "Error connecting to database", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    // empty on purpose
                 }
             });
         }
-
-        public void runInNewThread() {
-            new Thread(this).start();
-        }
-    }
-
-    private void startIntent(User user) {
-        Intent intentHome = new Intent(LoginActivity.this, HomeActivity.class);
-        intentHome.putExtra("USER_USERNAME", user.getUsername());
-        intentHome.putExtra("USER_NAME", user.getName());
-        intentHome.putExtra("USER_EMAIL", user.getEmail());
-        intentHome.putExtra("USER_PASSWORD", user.getPassword());
-        intentHome.putExtra("USER_TOKEN", user.getToken());
-        startActivity(intentHome);
     }
 }
