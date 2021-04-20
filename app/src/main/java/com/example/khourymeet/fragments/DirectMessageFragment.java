4892cfe92304f94b47e822d10e1ae0178f995b1a
marielle1022 <marielle.riveros@gmail.com>
2021-04-19 21:40:40 -0400

    updating db to add friend after first message sent; displays first message

--- app/src/main/java/com/example/khourymeet/fragments/DirectMessageFragment.java
+++ app/src/main/java/com/example/khourymeet/fragments/DirectMessageFragment.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.khourymeet.R;
@@ -22,6 +23,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -46,6 +49,11 @@ public class DirectMessageFragment extends Fragment {
     private final String defaultString = "default";
     // Name of person searched for
     private String name;
+
+    // Name of current user
+    private String currentUsername;
+
+    int messageNum;
 
     // Views for messages
     CardView cardViewMessage1;
@@ -90,11 +98,12 @@ public class DirectMessageFragment extends Fragment {
         // Referenced Android documentation to retrieve data from Shared Preferences
         sharedPreferences = this.getActivity().getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
         otherUsername = sharedPreferences.getString(getString(R.string.other_username_preferences_key), defaultString);
+        currentUsername = sharedPreferences.getString(getString(R.string.username_preferences_key), defaultString);
 
         databaseReference = FirebaseDatabase.getInstance().getReference();
 
-        getMessageViews();
-        createUser();
+        messageNum = 0;
+
     }
 
     @Override
@@ -102,6 +111,22 @@ public class DirectMessageFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_direct_message, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getMessageViews();
+        createUser();
+
+        sendButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View complete) {
+                if (messageNum == 0) {
+                    addFriend();
+                    setMessage1();
+                }
+            }
+        });
     }
 
     // Create User object from Database entry for the current username
@@ -116,7 +141,6 @@ public class DirectMessageFragment extends Fragment {
                     setMessageHeaderText(otherUser);
                 }
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
@@ -127,20 +151,20 @@ public class DirectMessageFragment extends Fragment {
     // TODO: error saying "null object reference"
     // Get views for messages
     private void getMessageViews() {
-        cardViewMessage1 = (CardView) getView().findViewById(R.id.dm_message1);
+        cardViewMessage1 = getView().findViewById(R.id.dm_message1);
         cardViewMessage1.setVisibility(View.GONE);
-//        message1 = getView().findViewById(R.id.out_text1);
-//
-//        cardViewMessage2 = getView().findViewById(R.id.dm_message2);
-//        cardViewMessage2.setVisibility(View.GONE);
-//        message2 = getView().findViewById(R.id.in_text);
-//
-//        cardViewMessage3 = getView().findViewById(R.id.dm_message3);
-//        cardViewMessage3.setVisibility(View.GONE);
-//        message3 = getView().findViewById(R.id.out_text2);
-//
-//        writeMessage = getView().findViewById(R.id.write_message);
-//        sendButton = getView().findViewById(R.id.send_button);
+        message1 = getView().findViewById(R.id.out_text1);
+
+        cardViewMessage2 = getView().findViewById(R.id.dm_message2);
+        cardViewMessage2.setVisibility(View.GONE);
+        message2 = getView().findViewById(R.id.in_text);
+
+        cardViewMessage3 = getView().findViewById(R.id.dm_message3);
+        cardViewMessage3.setVisibility(View.GONE);
+        message3 = getView().findViewById(R.id.out_text2);
+
+        writeMessage = getView().findViewById(R.id.write_message);
+        sendButton = getView().findViewById(R.id.send_button);
     }
 
     // Populate message screen with data from database
@@ -152,5 +176,42 @@ public class DirectMessageFragment extends Fragment {
         nameView.setText(name);
     }
 
+    // Add other user to list of friends
+    private void addFriend() {
+        databaseReference.child(getString(R.string.users_path,
+                currentUsername)).addListenerForSingleValueEvent(new ValueEventListener() {
+            // Use snapshot to create User object
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    User currentUser = snapshot.getValue(User.class);
+                    if (currentUser != null) {
+                        String currFriendsStr = currentUser.getFriends();
+                        if (currFriendsStr != null && !currFriendsStr.equals("")) {
+                            List<String> currFriendsList = currentUser.convertStrToArray(currFriendsStr);
+                            if (!currFriendsList.contains(otherUsername)) {
+                                currFriendsList.add(otherUsername);
+                                String newFriendsStr = currentUser.removeBracketsArrStr(currFriendsList.toString());
+                                databaseReference.child("users").child(currentUsername).child("friends").setValue(newFriendsStr);
+                            }
+                        } else {
+                            databaseReference.child("users").child(currentUsername).child("friends").setValue(otherUsername);
+                        }
+                    }
+                }
+                messageNum += 1;
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void setMessage1() {
+        String message = writeMessage.getText().toString();
+        message1.setText(message);
+        cardViewMessage1.setVisibility(View.VISIBLE);
+    }
 
 }
